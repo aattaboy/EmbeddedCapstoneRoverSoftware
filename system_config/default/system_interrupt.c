631b58e78578c43a5942f212776751d8fc66deeb
James Reed <jamesr66@vt.edu>
2016-04-01 22:52:29 -0400

    Initial commit

---
+++ system_config/default/system_interrupt.c
@@ -0,0 +1,33 @@
+#include <xc.h>
+#include <sys/attribs.h>
+#include "uart_receiver.h"
+#include "system_definitions.h"
+
+void IntHandlerDrvUsartInstance0(void)
+{
+  BaseType_t higherPriorityTaskWoken = pdFALSE;
+  
+  if (PLIB_INT_SourceFlagGet(INT_ID_0, INT_SOURCE_USART_1_TRANSMIT)) {
+    // Send message to transmitter to start transmitting again
+      writeToDebug(RECEIVER_ISR_BYTE);
+    if (PLIB_USART_TransmitterIsEmpty(USART_ID_1)) {
+      sendToFullBufferQueueFromISR(' ', &higherPriorityTaskWoken);
+      PLIB_INT_SourceDisable(INT_ID_0, INT_SOURCE_USART_1_TRANSMIT);
+      PLIB_INT_SourceFlagClear(INT_ID_0, INT_SOURCE_USART_1_TRANSMIT);
+    }
+  }
+  
+  if (PLIB_INT_SourceFlagGet(INT_ID_0, INT_SOURCE_USART_1_RECEIVE)) {
+    while (PLIB_USART_ReceiverDataIsAvailable(USART_ID_1)) {
+      uint8_t testChar = PLIB_USART_ReceiverByteReceive(USART_ID_1);
+
+      //sendToUartReceiverQueueFromISR(&testChar, &higherPriorityTaskWoken);
+
+      writeToDebug(RECEIVER_ISR_BYTE);
+    }
+    PLIB_INT_SourceFlagClear(INT_ID_0, INT_SOURCE_USART_1_RECEIVE);
+  }
+  PLIB_INT_SourceFlagClear(INT_ID_0, INT_SOURCE_USART_1_ERROR);
+
+  portEND_SWITCHING_ISR(higherPriorityTaskWoken);
+}
