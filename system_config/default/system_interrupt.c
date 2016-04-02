d381968d3c0d484380e74a511555467a239086d7
James Reed <jamesr66@vt.edu>
2016-04-02 18:15:29 -0400

    clang-format system interrupt file

--- system_config/default/system_interrupt.c
+++ system_config/default/system_interrupt.c
@@ -6,48 +6,45 @@
 #include "uart_receiver_public.h"
 #include "system_definitions.h"
 
-void IntHandlerDrvTmrInstance0(void)
-{
+void IntHandlerDrvTmrInstance0(void) {
   BaseType_t higherPriorityTaskWoken = pdFALSE;
-  
-  sendToEncodersQueueFromISR(LEFT, &higherPriorityTaskWoken);
-  PLIB_INT_SourceFlagClear(INT_ID_0,INT_SOURCE_TIMER_3);
-  
-  portEND_SWITCHING_ISR(higherPriorityTaskWoken);
-} 
 
-void IntHandlerDrvTmrInstance1(void)
-{
-  BaseType_t higherPriorityTaskWoken = pdFALSE;
-  
-  sendToEncodersQueueFromISR(RIGHT, &higherPriorityTaskWoken);
-  PLIB_INT_SourceFlagClear(INT_ID_0,INT_SOURCE_TIMER_4);
-  
+  sendToEncodersQueueFromISR(LEFT, &higherPriorityTaskWoken);
+  PLIB_INT_SourceFlagClear(INT_ID_0, INT_SOURCE_TIMER_3);
+
   portEND_SWITCHING_ISR(higherPriorityTaskWoken);
 }
 
-void IntHandlerDrvUsartInstance0(void)
-{
+void IntHandlerDrvTmrInstance1(void) {
   BaseType_t higherPriorityTaskWoken = pdFALSE;
-  
+
+  sendToEncodersQueueFromISR(RIGHT, &higherPriorityTaskWoken);
+  PLIB_INT_SourceFlagClear(INT_ID_0, INT_SOURCE_TIMER_4);
+
+  portEND_SWITCHING_ISR(higherPriorityTaskWoken);
+}
+
+void IntHandlerDrvUsartInstance0(void) {
+  BaseType_t higherPriorityTaskWoken = pdFALSE;
+
   if (PLIB_INT_SourceFlagGet(INT_ID_0, INT_SOURCE_USART_1_TRANSMIT)) {
     // Send message to transmitter to start transmitting again
-      writeToDebug(RECEIVER_ISR_BYTE);
+    writeToDebug(RECEIVER_ISR_BYTE);
     if (PLIB_USART_TransmitterIsEmpty(USART_ID_1)) {
-      //sendToFullBufferQueueFromISR(' ', &higherPriorityTaskWoken);
+      // sendToFullBufferQueueFromISR(' ', &higherPriorityTaskWoken);
       PLIB_INT_SourceDisable(INT_ID_0, INT_SOURCE_USART_1_TRANSMIT);
       PLIB_INT_SourceFlagClear(INT_ID_0, INT_SOURCE_USART_1_TRANSMIT);
     }
   }
-  
+
   if (PLIB_INT_SourceFlagGet(INT_ID_0, INT_SOURCE_USART_1_RECEIVE)) {
     while (PLIB_USART_ReceiverDataIsAvailable(USART_ID_1)) {
       uint8_t testChar = PLIB_USART_ReceiverByteReceive(USART_ID_1);
 
       sendToUartReceiverQueueFromISR(&testChar, &higherPriorityTaskWoken);
 
-      //writeToDebug(testChar);
-      //writeToDebug(RECEIVER_ISR_BYTE);
+      // writeToDebug(testChar);
+      // writeToDebug(RECEIVER_ISR_BYTE);
     }
     PLIB_INT_SourceFlagClear(INT_ID_0, INT_SOURCE_USART_1_RECEIVE);
   }
